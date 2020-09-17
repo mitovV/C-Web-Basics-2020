46aa49e04287dd661455bc8e920e4c08aea13c63
CodingMania <vasilmitovv@gmail.com>
2020-09-17 17:43:57 +0300

    Initial commit

---
+++ CustomHttpServer/CustomHttpServer/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CustomHttpServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            const int Port = 80;
+
+            var tcpListener = new TcpListener(IPAddress.Loopback, Port);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = await tcpListener.AcceptTcpClientAsync();
+
+                using var stream = client.GetStream();
+                var buffer = new byte[1000000];
+
+                var readedBytes = stream.Read(buffer, 0, buffer.Length);
+
+                var data = Encoding.UTF8.GetString(buffer,0,buffer.Length);
+
+                Console.WriteLine(data);
+                Console.WriteLine(new string('=', 80));
+            }
+        }
+    }
+}
