0c9256dc2f4468e28ce3da3c137b1a873d7649e6
CodingMania <vasilmitovv@gmail.com>
2020-09-18 10:40:04 +0300

    Implement method GetAllData

--- CustomHttpServer/CustomHttpServer/Program.cs
+++ CustomHttpServer/CustomHttpServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace CustomHttpServer
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Net;
     using System.Net.Sockets;
     using System.Text;
@@ -20,18 +22,43 @@
 
             while (true)
             {
-                var client = await tcpListener.AcceptTcpClientAsync();
+                var client = tcpListener.AcceptTcpClient();
+              
+                var readedBytes = GetAllData(client).ToArray();
 
-                using var stream = client.GetStream();
-                var buffer = new byte[1000000];
+                var dataAsString = Encoding.UTF8.GetString(readedBytes, 0, readedBytes.Length);
 
-                var readedBytes = stream.Read(buffer, 0, buffer.Length);
-
-                var data = Encoding.UTF8.GetString(buffer,0,buffer.Length);
-
-                Console.WriteLine(data);
+                Console.WriteLine(dataAsString);
                 Console.WriteLine(new string('=', 80));
             }
+        }
+
+        private static List<byte> GetAllData(TcpClient client)
+        {
+            using var stream = client.GetStream();
+
+            var data = new List<byte>();
+
+            var buffer = new byte[4096];
+
+            while (true)
+            {
+                var readedBytes = stream.Read(buffer, 0, buffer.Length);
+
+                if (readedBytes == 0 && buffer.Any(x => x != 0))
+                {
+                    break;
+                }
+
+                data.AddRange(buffer.Where(x => x != 0));
+
+                if (buffer.Where(x => x != 0).Count() < buffer.Length)
+                {
+                    break;
+                }
+            }
+
+            return data;
         }
     }
 }
