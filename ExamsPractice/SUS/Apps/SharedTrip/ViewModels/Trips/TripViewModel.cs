9fed6ad9378591b6902693049256294449a41852
CodingMania <vasilmitovv@gmail.com>
2020-10-23 17:52:19 +0300

    Implement TripViewModel

---
+++ ExamsPractice/SUS/Apps/SharedTrip/ViewModels/Trips/TripViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedTrip.ViewModels.Trips
+{
+    using System;
+
+    public class TripViewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPonit { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public byte Seats { get; set; }
+    }
+}
