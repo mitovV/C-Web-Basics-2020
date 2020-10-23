ac524d32377d663caefc923bc0997ef235305c2f
CodingMania <vasilmitovv@gmail.com>
2020-10-23 16:37:59 +0300

    Implement database logic users controller and service

---
+++ ExamsPractice/SUS/Apps/SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedTrip.Controllers
+{
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            return this.View();
+        }
+
+        public HttpResponse Register()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ ExamsPractice/SUS/Apps/SharedTrip/Data/Trip.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedTrip.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Trip
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public byte Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
+    }
+}
